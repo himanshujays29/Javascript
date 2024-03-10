ec99a2f4ff14dbbffef1a6b3a0be4681216f4d58
Himanshujays29 <himanshujayswal2006@gmail.com>
2024-03-10 21:02:49 +0530

    oprators in js

---
+++ frist.js
@@ -0,0 +1,36 @@
+// Arthmatic Oprators
+
+/**let a = 5;
+let b = 2;
+
+console.log("a =", a , "&  b =", b)
+console.log("a + b =", a+b)
+console.log("a - b =", a-b)
+console.log("a * b =", a*b)
+console.log("a / b =", a/b)
+console.log("a % b =", a%b)
+console.log("a ** b =", a**b)**/
+
+// Unary Oprators
+
+/*let a = 5;
+let b = 2;
+
+console.log("a =", a , "&  b =", b)
+console.log("a-- =",--a)
+console.log("a =", a) */
+
+//Assignment oprator 
+
+/*let a = 5;
+let b = 2;
+
+a = 10; //a = a + 25
+console.log("a = ", a);*/
+
+// comprision oprators
+
+let a = 5;
+let b = 5;
+ console.log("a == b", a == b)
+
