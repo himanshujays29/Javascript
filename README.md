523a361589a81e6091898c259ecf696a63b2f0df
Himanshujays29 <himanshujayswal2006@gmail.com>
2024-03-20 21:51:07 +0530

    for loop & while loop

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Javascript learning <br>
 Topics
 1. Oprators 
 2. Statements 
+3. loops 
+<br>
+Author :- Himanshu Jayswal 
--- loops.js
+++ loops.js
@@ -28,6 +28,83 @@ console.log ("sum = ", sum)
 console.log("Loop has ended");  */
 
 
-// for( let i = 1; i <= 10; i++) {
-//     console.log("i =", i);
-// }
+/* for( let i = 1; i <= 10; i++) {
+    console.log("hello");
+ }
+ console.log("Hii This Is Himanshu ");
+
+ */
+
+ //While loop
+
+/*
+let i = 1;
+while(i <= 5){
+    console.log("himanshu");
+    i++;
+}
+*/
+
+//Do While Loop
+
+/*
+
+let i = 1;
+do{
+    console.log("Apna Colege");
+    i++;
+} while(i<=10);
+
+*/
+
+//For Of loops 
+
+/*
+let str = "JavaScript";
+let size = 0;
+for(let val of str){
+    //iterator -> charecters
+    console.log("val =", val);
+    size++;
+}
+console.log("string size =", size); //10
+
+*/
+
+//For in loop
+/*
+let student = {
+    name: "Ram kumar",
+    age: 20,
+    cgpa: 7.5,
+    ispass: true
+};
+
+for(let key in student) {
+    console.log("key =", key, "value =", student[key]);
+}
+*/
+
+//Practice Question no. 1
+
+/* 
+for(let num = 1; num<=100; num++){
+    // console.log("num =", num)
+    if (num% 2==0){
+        console.log("odd num =", num);
+    }
+    else{
+        console.log("even num =", num);
+    }
+    }
+   */
+
+    //practice Question no. 2
+/*
+    let gamenum = 25;
+    let usernum = prompt("Guess the game number :");
+   while(usernum != gamenum) { 
+    usernum = prompt("You enterd wrong number Guess again :");
+   }
+console.log("Congratulation, you enterd a right number you win $10000 !Enjoy");
+*/
