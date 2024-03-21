6a424f5e1671285f640a56d50e8ffb4c04766c62
Himanshujays29 <himanshujayswal2006@gmail.com>
2024-03-21 21:28:59 +0530

    strings

---
+++ string.js
@@ -0,0 +1,28 @@
+//Strings
+
+let str = "himanshujayswal"; //Create
+console.log(str.length); //Length
+console.log(str[11]); //Indices
+
+
+//Tamplate Literals
+
+let sentence = `This is a tamplate litrel` ;
+console.log(sentence);
+
+//Example
+
+let object = {
+    item: "pen",
+    price: 10,
+};
+
+let output = `The cost of ${object.item} is ${object.price} rupees`;//tamplate litrels
+console.log(output);
+console.log("The cost of", object.item, "is", object.price ,"rupees"); //Normal
+
+//Escape
+
+console.log("Himanshu\njayswal"); // \n next line
+console.log("Himanshu\tjayswal"); // \t tab
+    
